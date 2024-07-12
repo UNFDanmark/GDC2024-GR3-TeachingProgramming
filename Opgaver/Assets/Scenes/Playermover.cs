792afc2f518a37c292f7fd9f6e74bbd2173e5e8b
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-12 15:28:42 +0200

    tilføj hastighed integer

--- Opgaver/Assets/Scenes/Playermover.cs
+++ Opgaver/Assets/Scenes/Playermover.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Playermover : MonoBehaviour
 {
+    public int hastighed = 5; 
+    
     // Start is called before the first frame update
     void Start()
     {
         print("jeg har kaldt en gang");
+        print(hastighed);
     }
 
     // Update is called once per frame
