dd20477bd84dc9a8952e77154c3b480b329a0773
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-12 16:40:48 +0200

    Added player rotation and split the player logic and the player grapics

--- Opgaver/Assets/Scenes/Bodyrotation.cs
+++ Opgaver/Assets/Scenes/Bodyrotation.cs
@@ -1,22 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mail;
 using UnityEngine;
 
-public class Playermover : MonoBehaviour
+public class Bodyrotation : MonoBehaviour
 {
-    public float hastighed = 5f;
-    
     
     // Start is called before the first frame update
     void Start()
     {
-        print("jeg har kaldt en gang");
-        print(hastighed);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("jeg er kaldt meget");
+        float TurnaroundInput = Input.GetAxis("Turnaround");
+        transform.Rotate(0, TurnaroundInput, 0);
     }
 }
