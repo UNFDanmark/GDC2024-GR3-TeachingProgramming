c3e4ce23188b1a6a947cbadb2f001b47227fbe15
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-12 14:50:38 +0200

    Lavet player fjende og gulv

---
+++ Opgaver/Assets/Scenes/Playermover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("jeg har kaldt en gang");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("jeg er kaldt meget");
+    }
+}
