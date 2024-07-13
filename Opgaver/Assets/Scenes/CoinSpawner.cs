8532be72a638fa3ecfd4449de3d1a28058eee51d
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-13 12:12:21 +0200

    tilføjede coinspawner

---
+++ Opgaver/Assets/Scenes/CoinSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+    private float leftovercooldown;
+    public float EnenmyCooldown = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftovercooldown = EnenmyCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftovercooldown = leftovercooldown - Time.deltaTime;
+        if (leftovercooldown <= 0) 
+        {
+            float xSpawn = Random.Range(xMin, xMax);
+            float zSpawn = Random.Range(zMin, zMax);    
+            Vector3 SpawnPoint = new Vector3(xSpawn, transform.position.y, zSpawn);
+            Instantiate(coinPrefab, SpawnPoint, Quaternion.identity);
+            leftovercooldown = EnenmyCooldown;
+        }
+    }
+}
