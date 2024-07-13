5975162e776f95a6ffc6d4ea9b78954f7f7400ec
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-13 11:48:35 +0200

    tilføjet enemy movement

--- Opgaver/Assets/Scenes/EnemyCode.cs
+++ Opgaver/Assets/Scenes/EnemyCode.cs
@@ -2,20 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyCode : MonoBehaviour
 {
+    public NavMeshAgent agent;
+
+    private GameObject player;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        agent.SetDestination(player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 
     private void OnCollisionEnter(Collision other)
