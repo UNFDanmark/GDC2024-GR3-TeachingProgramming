0301368ba1a91cec93ed8feb2e1d2bd4b1f10635
Deltager <teknisk+GDC2024-GR8@game.unf.dk>
2024-07-14 12:31:43 +0200

    Tilføjet liv counter

--- Opgaver/Assets/Scenes/Playermover.cs
+++ Opgaver/Assets/Scenes/Playermover.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Playermover : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class Playermover : MonoBehaviour
     private Rigidbody rb;
     public Animator animator;
     public GameObject gameOverScreen;
+    public float Liv = 3f;
+    public TextMeshProUGUI LivTekst;
     
     // Start is called before the first frame update
     void Start()
@@ -25,12 +29,28 @@ public class Playermover : MonoBehaviour
         rb.velocity = movement;
         
         animator.SetFloat("Speed", rb.velocity.magnitude);
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("test");
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("Enemy"))
-            gameOverScreen.SetActive(true);
+            if (Liv <= 0)
+            {
+                gameOverScreen.SetActive(true);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Liv -= 1;
+                LivTekst.text = "Liv: " + Liv.ToString() + "/3";
+                
+            }
     }
+    
 }
+
 
